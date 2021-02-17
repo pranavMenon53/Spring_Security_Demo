cd5b31ecf834741c99d49d32048dc4cb55614347
pranavMenon53 <pranavmenon0810@gmail.com>
2021-02-17 22:14:03 +0530

    Using cusom user and Implemented PasswordEncoder

--- src/main/java/com/spring_security_project/spring_security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/spring_security_project/spring_security/security/ApplicationSecurityConfig.java
@@ -1,13 +1,25 @@
 package com.spring_security_project.spring_security.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final PasswordEncoder passwordEncoder;
+
+  public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+    this.passwordEncoder = passwordEncoder;
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -19,5 +31,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
       .authenticated()
       .and()
       .httpBasic();
+  }
+
+  @Override
+  @Bean
+  protected UserDetailsService userDetailsService() {
+    UserDetails user = User
+      .builder()
+      .username("David")
+      //   .password("Pas$WorD")
+      .password(passwordEncoder.encode("Pas$WorD"))
+      .roles("STUDENT")
+      .build();
+
+    return new InMemoryUserDetailsManager(user);
   }
 }
